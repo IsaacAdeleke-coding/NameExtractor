2f60832c7c627dc73396a31aa24f8d2a158203d2
IsaacAdeleke-coding <Isaacadelek3@gmail.com>
2024-10-20 11:21:21 -0700

    Initial Commit

---
+++ NameExtractor/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  NameExtractor
+//
+//  Created by Isaac Adeleke on 10/20/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
